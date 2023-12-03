43deb5584d78293f303b744aac0f12da1b3e5d50
vietpham-stvg <82923647+phamquangviet891@users.noreply.github.com>
2023-12-03 22:41:37 +0700

    update
    dashboard/ chart atttendance week

--- se100_cs/se100_cs/APIs/MyAttendance.cs
+++ se100_cs/se100_cs/APIs/MyAttendance.cs
@@ -45,7 +45,7 @@ namespace se100_cs.APIs
             return "Absent";
         }
         public async Task<string> markAttendance(long id)
-            {
+        {
             string status;
             DateTime now = DateTime.UtcNow;
             int today = now.Day;
@@ -63,22 +63,22 @@ namespace se100_cs.APIs
                 int start_time_hour = setting.start_time_hour;
                 int start_time_minute = setting.start_time_minute;
 
-                SqlEmployee employee = context.employees!.Where(s=>s.ID == id).FirstOrDefault();
+                SqlEmployee employee = context.employees!.Where(s => s.ID == id).FirstOrDefault();
                 if (employee == null)
                 {
                     return "employee null";
                 }
-                SqlAttendance existing = context.attendances!.Include(s=>s.employee).Where(s=>s.employee.ID== id &&s.time.Day==today).FirstOrDefault();
+                SqlAttendance existing = context.attendances!.Include(s => s.employee).Where(s => s.employee.ID == id && s.time.Day == today).FirstOrDefault();
 
                 if (existing != null)
                 {
                     status = attendance_status(existing.status);
-                    return status ;
+                    return status;
                 }
                 SqlAttendance sqlAttendance = new SqlAttendance();
                 sqlAttendance.employee = employee;
                 sqlAttendance.time = now;
-                if(now.Hour*60+now.Minute>start_time_hour*60+start_time_minute)
+                if (now.Hour * 60 + now.Minute > start_time_hour * 60 + start_time_minute)
                 {
                     sqlAttendance.status = 1;
                 }
@@ -90,7 +90,7 @@ namespace se100_cs.APIs
                 status = attendance_status(sqlAttendance.status);
                 try
                 {
-                int rows = await context.SaveChangesAsync();
+                    int rows = await context.SaveChangesAsync();
                 }
                 catch (Exception ex)
                 {
@@ -102,6 +102,8 @@ namespace se100_cs.APIs
         }
         public class Employees_Today
         {
+            public int thu { get; set; }
+            public int ngay { get; set; }
             public int attendance { get; set; } = 0;
             public int late_coming { get; set; } = 0;
             public int absent { get; set; } = 0;
@@ -111,37 +113,43 @@ namespace se100_cs.APIs
         public List<Employees_Today> getEmployees_ByWeek()
         {
             int ngay_hom_nay = DateTime.Now.Day;
+            int thang_truoc = DateTime.Now.Month - 1;
             List<Employees_Today> week = new List<Employees_Today>();
-            int thumay = (int)DateTime.Now.DayOfWeek; // vi du thu 5
-            for(int i =thumay; i>=2 ; i--)
+            int hom_nay_la_thu = (int)DateTime.Now.DayOfWeek + 1; // vi du thu 5, thứ 2 là bằng 2
+            if (hom_nay_la_thu == 1) { hom_nay_la_thu = 8; }
+            for (int i = hom_nay_la_thu; i >= 2; i--)
             {
                 Employees_Today today = new Employees_Today();
                 using (DataContext context = new DataContext())
                 {
-                    int attendance = context.attendances.Where(s => s.status == 0 && s.time.Day == ngay_hom_nay-(thumay-i)).Count();
-                    int late_coming = context.attendances!.Where(s => s.status == 1 && s.time.Day == ngay_hom_nay - (thumay - i)).Count();
+                    if (ngay_hom_nay - (hom_nay_la_thu - i) < 1)
+                    {
+                        if (thang_truoc % 2 == 1)
+                        {
+                            ngay_hom_nay = 30;
+                        }
+                        else
+                        {
+                            ngay_hom_nay = 31;
+                        }
+                        hom_nay_la_thu = i;
+                    }
+                    int attendance = context.attendances.Where(s => s.status == 0 && s.time.Day == ngay_hom_nay - (hom_nay_la_thu - i)).Count();
+                    int late_coming = context.attendances!.Where(s => s.status == 1 && s.time.Day == ngay_hom_nay - (hom_nay_la_thu - i)).Count();
                     int total = Program.api_employee.countTotalEmployee();
                     int absent = total - attendance - late_coming;
+                    today.thu = i;
+                    today.ngay = ngay_hom_nay - (hom_nay_la_thu - i);
                     today.attendance = attendance;
                     today.absent = absent;
                     today.late_coming = late_coming;
                     week.Add(today);
                 }
-
-            }    
-            using (DataContext context = new DataContext())
-            {
-                int attendance = context.attendances.Where(s => s.status == 0 && s.time.Day == day).Count();
-                int late_coming = context.attendances!.Where(s => s.status == 1 && s.time.Day == day).Count();
-                int total = Program.api_employee.countTotalEmployee();
-                int absent = total - attendance - late_coming;
-                today.attendance = attendance;
-                today.absent = absent;
-                today.late_coming = late_coming;
             }
+            week = week.OrderBy(s => s.thu).ToList();
             return week;
         }
-        public Employees_Today getEmployees_Byday(int day) 
+        public Employees_Today getEmployees_Byday(int day)
         {
             Employees_Today today = new Employees_Today();
             int count_attendance = 0;
@@ -161,16 +169,16 @@ namespace se100_cs.APIs
         {
             int day = DateTime.Now.Day;
             Employees_Today today = new Employees_Today();
-            int count_attendance=0;
-            using(DataContext context = new DataContext())
+            int count_attendance = 0;
+            using (DataContext context = new DataContext())
             {
-                int attendance = context.attendances.Where(s=>s.status==0 && s.time.Day==day).Count();
-                int late_coming = context.attendances!.Where(s=>s.status==1 && s.time.Day == day).Count();
+                int attendance = context.attendances.Where(s => s.status == 0 && s.time.Day == day).Count();
+                int late_coming = context.attendances!.Where(s => s.status == 1 && s.time.Day == day).Count();
                 int total = Program.api_employee.countTotalEmployee();
                 int absent = total - attendance - late_coming;
                 today.attendance = attendance;
                 today.absent = absent;
-                today.late_coming= late_coming;
+                today.late_coming = late_coming;
             }
             return today;
         }
--- se100_cs/se100_cs/APIs/MyDashboard.cs
+++ se100_cs/se100_cs/APIs/MyDashboard.cs
@@ -10,21 +10,11 @@
             public long max { set; get; } = 0;
             public long total { set; get; } = 0;
         }
-        //public class Attendance_byWeek
-        //{
-        //    public MyAttendance.Employees_Today monday = new MyAttendance.Employees_Today();
-        //    public MyAttendance.Employees_Today tuesday = new MyAttendance.Employees_Today();
-        //    public MyAttendance.Employees_Today wednesday = new MyAttendance.Employees_Today();
-        //    public MyAttendance.Employees_Today thurday= new MyAttendance.Employees_Today();
-        //    public MyAttendance.Employees_Today friday= new MyAttendance.Employees_Today();
-        //    public MyAttendance.Employees_Today saturday= new MyAttendance.Employees_Today();
-        //    public MyAttendance.Employees_Today sunday= new MyAttendance.Employees_Today();
-        //}
         protected class Response_DTO
         {
             public MyAttendance.Employees_Today employees_Today { get; set; }
             public MyEmployee.Total_Employee total_Employee { get; set; }
-            public MyAttendance.Attendance_byWeek attendance_ByWeek { get; set; }
+            public List<MyAttendance.Employees_Today> attendance_ByWeek { get; set; }
             public Salary_byMonth salary_ByMonth { get; set; } = new Salary_byMonth();
 
         }
@@ -33,7 +23,7 @@
             MyAttendance.Employees_Today employees_Today = Program.api_attendance.getEmployees_Today();
             Salary_byMonth salary_ByMonth = new Salary_byMonth();
             MyEmployee.Total_Employee total_Employee = Program.api_employee.getTotal_Employee();
-            MyAttendance.Attendance_byWeek attendance_ByWeek = Program.api_attendance.getEmployees_ByWeek();
+            List<MyAttendance.Employees_Today> attendance_ByWeek = Program.api_attendance.getEmployees_ByWeek();
             Response_DTO response_DTO = new Response_DTO();
             response_DTO.employees_Today = employees_Today;
             response_DTO.total_Employee = total_Employee;
